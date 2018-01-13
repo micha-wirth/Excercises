c358cfad784366bececd8a95e8b42d3be6e30eb8
micha-wirth <mchwrth@outlook.de>
2018-01-13 22:42:00 +0100

    Initial commit

--- heap_sort.py
+++ heap_sort.py
@@ -4,6 +4,9 @@
 def heap_sort(array):
     """ Implementation of the heap sort algorithm.
 
+    :param arr:
+    :return:
+
     # List with an even number ob items to function call.
     >>> heap_sort([0, -1, 5, -2])
     [-2, -1, 0, 5]
@@ -19,19 +22,28 @@ def heap_sort(array):
     # Empty list to function call.
     >>> heap_sort([])
     []
-
-    :param arr:
-    :return:
     """
-    root_parent_index = 0
-    # initial heapify operation.
+    # # Option #1:
+    # root_parent_index = 0
+    # last_child_index = len(array) - 1
+    # # initial heapify operation.
+    # heapify(array)
+    # for child_index in range(last_child_index, root_parent_index, -1):
+    #     # Swap the last item with the first item.
+    #     array[root_parent_index], array[child_index] = array[child_index], array[root_parent_index]
+    #     # Repair heap from bottom up.
+    #     repair_heap(array, root_parent_index, child_index - 1)
+
+    # Option #2:
+    first_index = 0
+    first_child_index = 1
+    last_child_index = len(array) - 1
+
     heapify(array)
-    #
-    for child_index in range(len(array) - 1, root_parent_index, -1):
-        # Swap the last item with the first item.
-        array[root_parent_index], array[child_index] = array[child_index], array[root_parent_index]
-        # Repair heap from bottom up.
-        repair_heap(array, root_parent_index, child_index - 1)
+
+    for last_index in reversed(range(first_child_index, last_child_index + 1)):
+        array[first_index], array[last_index] = array[last_index], array[first_index]
+        repair_heap(array, first_index, last_index - 1)
 
     return array
 
@@ -41,13 +53,56 @@ def heapify(array):
 
     :param array:
     :return:
+
+    # List with an even number of elements to function call.
+    >>> heapify([0, 1, 2, 3, 4, 5])
+    [5, 4, 2, 3, 1, 0]
+
+    # List with an uneven number of elements to function call.
+    >>> heapify([0, 1, 2])
+    [2, 1, 0]
+
+    # List with only 1 element to function call.
+    >>> heapify([0])
+    [0]
+
+    # Empty list to function call.
+    >>> heapify([])
+    []
+
     """
+    # # Option #1:
+    # last_child_index = len(array) - 1
+    # last_parent_index = (last_child_index - 1) // 2
+    #
+    # for parent_index in range(last_parent_index, -1, -1):
+    #     repair_heap(array, parent_index, last_child_index)
 
-    last_child_index = len(array) - 1
-    last_parent_index = (last_child_index - 1) // 2
+    # Option #2:
+    if array:
+        last_child = len(array) - 1
+        last_parent = (last_child - 1) >> 1 # parent = (child - 1) // 2
+        if (last_child % 2) == 0:
+            right = last_child
+            left = right - 1
+            if array[right] > array[left] and array[right] > array[last_parent]:
+                array[right], array[last_parent] = array[last_parent], array[right]
+            elif array[left] > array[right] and array[left] > array[last_parent]:
+                array[left], array[last_parent] = array[last_parent], array[left]
+        else:
+            left = last_child
+            if array[left] > array[last_parent]:
+                array[left], array[last_parent] = array[last_parent], array[left]
 
-    for parent_index in range(last_parent_index, -1, -1):
-        repair_heap(array, parent_index, last_child_index)
+        for parent in range(last_parent - 1, -1, -1):
+            left = (parent << 1) + 1 # left = (2 * parent) + 1
+            right = left + 1
+            if array[left] > array[right] and array[left] > array[parent]:
+                array[left], array[parent] = array[parent], array[left]
+                repair_heap(array, left, last_child)
+            elif array[right] > array[left] and array[right] > array[parent]:
+                array[right], array[parent] = array[parent], array[right]
+                repair_heap(array, right, last_child)
 
     return array
 
@@ -67,22 +122,44 @@ def repair_heap(array, parent_index, last_child_index):
     [4, 3, 0, 2, 1]
 
     """
-    current_parent_index = parent_index
-    heap_size = last_child_index + 1
+    # # Option #1:
+    # current_parent_index = parent_index
+    # heap_size = last_child_index + 1
+    #
+    # while current_parent_index < heap_size:
+    #     left_child_index = 2 * current_parent_index + 1
+    #     right_child_index = left_child_index + 1
+    #     max_child_index = left_child_index
+    #     if left_child_index > last_child_index:
+    #         break
+    #     if right_child_index < heap_size and array[right_child_index] > array[left_child_index]:
+    #         max_child_index = right_child_index
+    #     if array[max_child_index] > array[current_parent_index]:
+    #         array[max_child_index], array[current_parent_index] = array[current_parent_index], array[max_child_index]
+    #         current_parent_index = max_child_index
+    #     else:
+    #         break
 
-    while current_parent_index < heap_size:
-        left_child_index = 2 * current_parent_index + 1
-        right_child_index = left_child_index + 1
-        max_child_index = left_child_index
-        if left_child_index > last_child_index:
-            break
-        if right_child_index < heap_size and array[right_child_index] > array[left_child_index]:
-            max_child_index = right_child_index
-        if array[max_child_index] > array[current_parent_index]:
-            array[max_child_index], array[current_parent_index] = array[current_parent_index], array[max_child_index]
-            current_parent_index = max_child_index
-        else:
-            break
+    # Option #2:
+    import math
+
+    current_depth = int(math.log2(parent_index + 1))
+    max_depth = int(math.log2(last_child_index + 1))
+    parent = parent_index
+
+    for depth in range(current_depth, max_depth):
+        left = (parent << 1) + 1
+        right = left + 1
+        if right <= last_child_index:
+            if array[left] > array[right] and array[left] > array[parent]:
+                array[left], array[parent] = array[parent], array[left]
+                parent = left
+            elif array[right] > array[left] and array[right] > array[parent]:
+                array[right], array[parent] = array[parent], array[right]
+                parent = right
+        elif left <= last_child_index:
+            if array[left] > array[parent]:
+                array[left], array[parent] = array[parent], array[left]
 
     return array
 

a31670cc68042796b2a0c8f107fb0f24452ef62d
micha-wirth <mchwrth@outlook.de>
2018-03-10 17:11:11 +0100

    Initial commit

--- route_planner.py
+++ route_planner.py
@@ -36,7 +36,7 @@ class Graph:
 
         # Test
         >>> graph = Graph()
-        >>> graph.read_graph_from_file('test.graph')
+        >>> graph.read_graph_from_file('graph_13/test.graph')
         >>> graph
         [0->1(30), 0->(70), 1->2(20), 2->3(50), 3->1(40), 4->3(20)]
 
@@ -51,7 +51,7 @@ class Graph:
                     continue
                 column_lines += 1
                 if column_lines == 1:
-                    if self._num_nodes != 0
+                    if self._num_nodes != 0:
                         raise Exception('Graph is already read in')
                     self._num_nodes = int(columns[0])
                 elif column_lines == 2:
@@ -73,6 +73,138 @@ class Graph:
                               int(columns[3]))
                     # Append arc to tail node's adjacency list.
                     self._adjacency_lists[tail_node_id].append(arc)
+
+    def get_num_nodes(self):
+        """
+        :return: number of nodes in graph.
+        """
+        return self._num_nodes
+
+    def get_num_arcs(self):
+        """
+        :return: number of arcs in graph.
+        """
+        return self._num_arcs
+
+    def compute_reachable_nodes(self, node_id):
+        """
+        Mark all reachable nodes from a given node.
+        Implemented as breadth first search (BFS).
+
+        :param node_id: identifier of a given node.
+        :return: number of reachable start nodes (incl. start node).
+
+        >>> graph = Graph()
+        >>> graph.read_graph_from_file('graph_13/test2.graph')
+        >>> graph.compute_reachable_nodes(0)
+        4
+        >>> graph.compute_reachable_nodes(4)
+        6
+        >>> graph.compute_reachable_nodes(6)
+        1
+        """
+
+        # List of currently visited nodes.
+        current_level = [node_id]
+        # Create a list of marked nodes. Reachable nodes are marked with 1.
+        marked_nodes = [0] * self._num_nodes
+        # Mark start node as reachable.
+        marked_nodes[node_id] = 1
+        # Store number of reachable nodes.
+        num_marked_nodes = 1
+        # Visit all nodes.
+        while len(current_level) > 0:
+            # Store nodes which are connected to current_level nodes.
+            next_level = []
+            # Go through all nodes of current_level.
+            for curr_node_id in current_level:
+                # Go through arcs of current node:
+                for arc in self._adjacency_lists[curr_node_id]:
+                    # If had_id has not been marked yet.
+                    if not marked_nodes[arc.head_node_id]:
+                        marked_nodes[arc.head_node_id] = 1
+                        num_marked_nodes += 1
+                        # Add head_id to the new current level nodes.
+                        next_level.append(arc.head_node_id)
+            current_level = next_level
+        return num_marked_nodes
+
+    def set_arc_costs_to_travel_time(self, max_vehicle_speed):
+        """
+        Set arc costs to travel time in whole seconds.
+
+        :param self:
+        :param max_vehicle_speed: [km/h]
+        :return: None
+
+        >>> graph = Graph()
+        >>> graph.read_graph_from_file('graph_13/test.graph')
+        >>> graph
+        [0->1(30), 0->2(70), 1->2(20), 2->3(50), 3->1(40), 4->3(20)]
+        """
+
+        for i in range(self._num_nodes):
+            for arc in self._adjacency_lists[i]:
+                arc.costs = arc.distance
+
+    def compute_lcc(self, marked_nodes):
+        """
+        Mark all nodes in the largest connected component.
+        :param marked_nodes:
+        :return:
+        """
+
+    def compute_shortest_paths(self, start_node_id):
+        """
+        Compute the shortest paths for a given start node.
+        To solve this problem the Dijkstra's algorithm is used.
+        :param start_node_id:
+        :return:
+        """
+
+    def __repr__(self):
+        """
+        Define object's string representation.
+
+        :return: object represented as string.
+
+        >>> graph = Graph()
+        >>> graph.read_graph_from_file('graph_13/test.graph')
+        >>> graph
+        [0->1(30), 0->2(70), 1->2(20), 2->3(50), 3->1(40), 4->3(20)]
+        """
+
+        obj_str_repr = ""
+        for i in range(self._num_nodes):
+            for arc in self._adjacency_lists[i]:
+                obj_str_repr += repr(arc) + ", "
+            if obj_str_repr:
+                return "[" + obj_str_repr[:-2] + "]"
+            else:
+                return "[]"
+
+
+class Node:
+    def __init__(self, node_id, latitude, longitude):
+        self._id = node_id
+        self._latitude = latitude
+        self._longitude = longitude
+
+    def __repr__(self):
+        return "{0}".format(self._id)
+
+
+class Arc:
+    def __init__(self, tail_id, head_id, distance, max_speed):
+        self.tail_node_id = tail_id
+        self.head__node_id = head_id
+        self.distance = distance
+        self.max_speed = max_speed
+        self.costs = distance
+
+    def __repr__(self):
+        return "{0}->{1}({2}".format(self.tail_node_id, self.head__node_id,
+                                     self.costs)
 
 
 def main():
